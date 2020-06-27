b3a972db89442c58253a1b275e6aaed06df71589
eemrys <49338231+eemrys@users.noreply.github.com>
2020-06-27 11:48:08 +0300

    codelabs

--- codelabs/src/example/Fish.kt
+++ codelabs/src/example/Fish.kt
@@ -1,6 +1,6 @@
 package example
 
-data class Fish(val name: String)
+data class Fish(var name: String)
 
 fun filterLambda() {
     val myFish = listOf(Fish("Flipper"), Fish("Moby Dick"), Fish("Dory"))
@@ -10,6 +10,31 @@ fun filterLambda() {
 
 fun fishExamples() {
     val fish = Fish("splashy")  // all lowercase
+    myWith (fish.name) {    // with myWith() inline, this becomes a direct call: fish.name.capitalize()
+        println(capitalize())
+    }
+
+    // returns result of the lambda
+    fish.run {
+        name
+    }
+
+    //returns the changed object
+    val fish2 = Fish(name = "splashy").apply {
+        name = "sharky"
+    }
+    println(fish2)
+
+    // returns a copy of the object with the changes
+    println(fish.let { it.name.capitalize()}
+        .let{it + "fish"}
+        .let{it.length}
+        .let{it + 31})
+    println(fish) // the obj itself has not changed
+}
+
+inline fun myWith(name: String, block: String.() -> Unit) {
+    name.block()
 }
 
 fun main() {
