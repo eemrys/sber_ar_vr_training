e467061d4ed3b5863b6257f69f3d89ac5287293e
eemrys <49338231+eemrys@users.noreply.github.com>
2020-06-29 12:51:37 +0300

    Added EditText, Done Button, ClickHandler

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnDone.setOnClickListener() {
+            // Hide the keyboard.
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+            txtvNickname.text = eTxtName.text
+            eTxtName.visibility = View.GONE
+            txtvNickname.visibility = View.VISIBLE
+        }
+
     }
 }
