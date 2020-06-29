48684acce55ba7119b5898203d7b8a2e38889988
eemrys <49338231+eemrys@users.noreply.github.com>
2020-06-29 13:34:23 +0300

    Created addNickname function, implemented Data Binding

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -5,20 +5,35 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+    private val myName: MyName = MyName("Kelly George")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnDone.setOnClickListener() {
-            // Hide the keyboard.
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
-            txtvNickname.text = eTxtName.text
+        binding.myName = myName
+        binding.btnDone.setOnClickListener() {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        binding.apply {
+            myName?.nickname = eTxtName.text.toString()
+            // Invalidate all binding expressions and request a new rebind to refresh
+            invalidateAll()
             eTxtName.visibility = View.GONE
+            btnDone.visibility = View.GONE
             txtvNickname.visibility = View.VISIBLE
         }
 
+        // Hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
