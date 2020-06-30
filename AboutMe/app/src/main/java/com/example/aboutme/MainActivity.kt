2870061d9f7a0eed8b22c6e6e4ef04bcb3e54899
eemrys <49338231+eemrys@users.noreply.github.com>
2020-06-30 13:25:21 +0300

    AboutMe: changed to binding.apply, added separate hideKeyboard function

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -13,26 +13,35 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
-    private val myName: MyName = MyName("Kelly George")
+
+    private val newName: MyName = MyName("Kelly George")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.myName = myName
-        binding.btnDone.setOnClickListener() {
-            addNickName(it)
+        binding.apply {
+            myName = newName
+            btnDone.setOnClickListener {
+                addNickName()
+                hideKeyboard(it)
+            }
         }
     }
 
-    private fun addNickName(view: View) {
+    private fun addNickName() {
         binding.apply {
             myName?.nickname = eTxtName.text.toString()
+
             // Invalidate all binding expressions and request a new rebind to refresh
             invalidateAll()
+
             eTxtName.visibility = View.GONE
             btnDone.visibility = View.GONE
             txtvNickname.visibility = View.VISIBLE
         }
 
-        // Hide the keyboard.
+    }
+
+    private fun hideKeyboard(view: View) {
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
