1b1edc9799b16e9b80fef809e0206202c0c21b3b
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-02 15:18:12 +0300

    Added and rearranged methods in MainActivity, created separate Dessert data class

---
+++ DessertPusher/app/src/main/java/com/example/android/dessertpusher/DessertClass.kt
@@ -0,0 +1,3 @@
+package com.example.android.dessertpusher
+
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
--- DessertPusher/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
+++ DessertPusher/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
@@ -22,34 +22,14 @@ import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import timber.log.Timber
 
-/**
- * This is a class representing a timer that you can start or stop. The secondsCount outputs a count of
- * how many seconds since it started, every one second.
- *
- * -----
- *
- * Handler and Runnable are beyond the scope of this lesson. This is in part because they deal with
- * threading, which is a complex topic that will be covered in a later lesson.
- *
- * If you want to learn more now, you can take a look on the Android Developer documentation on
- * threading:
- *
- * https://developer.android.com/guide/components/processes-and-threads
- *
- */
 class DessertTimer(lifecycle: Lifecycle) : LifecycleObserver {
 
-    // The number of seconds counted since the timer started
     var secondsCount = 0
 
     init {
         lifecycle.addObserver(this)
     }
 
-    /**
-     * [Handler] is a class meant to process a queue of messages (known as [android.os.Message]s)
-     * or actions (known as [Runnable]s)
-     */
     private var handler = Handler()
     private lateinit var runnable: Runnable
 
