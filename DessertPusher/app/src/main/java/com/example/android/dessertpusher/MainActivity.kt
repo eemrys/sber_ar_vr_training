89e27cb0fc03ce8672ce717a89ba3c94e3edf0be
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-03 15:22:25 +0300

    Rearranged methods

--- DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -55,18 +55,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         Timber.i("onCreate called")
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.btnShareMenu -> onShare()
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
     override fun onRestart() {
         super.onRestart()
         Timber.i("onRestart called")
@@ -75,12 +63,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStart() {
         super.onStart()
         Timber.i("onStart called")
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-
-        Timber.i("onRestoreInstanceState called")
     }
 
     override fun onResume() {
@@ -98,6 +80,11 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         Timber.i("onStop called")
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy called")
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putInt(KEY_REVENUE, revenue)
@@ -107,9 +94,22 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         Timber.i("onSaveInstanceState called")
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Timber.i("onDestroy called")
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        Timber.i("onRestoreInstanceState called")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.btnShareMenu -> onShare()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun onDessertClicked() {
