98ed16568f0843f5914bb3ed687e7c76e3bc21e0
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-08 00:18:30 +0300

    removed data binding, changed xml IDs

--- MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -17,24 +17,15 @@
 package com.example.android.marsrealestate.detail
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
-import com.example.android.marsrealestate.databinding.FragmentDetailBinding
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.marsrealestate.R
 
-/**
- * This [Fragment] will show the detailed information about a selected piece of Mars real estate.
- */
-class DetailFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+class DetailFragment : Fragment(R.layout.fragment_detail) {
 
-        @Suppress("UNUSED_VARIABLE")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val application = requireNotNull(activity).application
-        val binding = FragmentDetailBinding.inflate(inflater)
-        binding.setLifecycleOwner(this)
-        return binding.root
     }
 }
--- MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -20,43 +20,22 @@ package com.example.android.marsrealestate.overview
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.marsrealestate.R
-import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
+import kotlinx.android.synthetic.main.fragment_overview.*
 
-/**
- * This fragment shows the the status of the Mars real-estate web services transaction.
- */
-class OverviewFragment : Fragment() {
+class OverviewFragment : Fragment(R.layout.fragment_overview) {
 
-    /**
-     * Lazily initialize our [OverviewViewModel].
-     */
     private val viewModel: OverviewViewModel by lazy {
-        ViewModelProviders.of(this).get(OverviewViewModel::class.java)
+        ViewModelProvider(this).get(OverviewViewModel::class.java)
     }
 
-    /**
-     * Inflates the layout with Data Binding, sets its lifecycle owner to the OverviewFragment
-     * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
-     */
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val binding = FragmentOverviewBinding.inflate(inflater)
-
-        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
-        binding.setLifecycleOwner(this)
-
-        // Giving the binding access to the OverviewViewModel
-        binding.viewModel = viewModel
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
-        return binding.root
+        txtvResponse.text = viewModel.response.value
     }
 
-    /**
-     * Inflates the overflow menu that contains filtering options.
-     */
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.overflow_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
