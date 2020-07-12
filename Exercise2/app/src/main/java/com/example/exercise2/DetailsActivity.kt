177ddd541ac1bb5ab5ee407d8317888ca0398c9a
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-12 21:33:09 +0300

    added click handler to go to url

--- Exercise2/app/src/main/java/com/example/exercise2/DetailsActivity.kt
+++ Exercise2/app/src/main/java/com/example/exercise2/DetailsActivity.kt
@@ -1,11 +1,17 @@
 package com.example.exercise2
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_details.*
 
-class DetailsActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity(R.layout.activity_details) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        btnTrailer.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/dQw4w9WgXcQ"))
+            startActivity(intent)
+        }
     }
 }
