091c3c31d1e73be7b45d3ad4bf2aa61e784951f8
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-12 17:45:50 +0300

    added onclicklistener to open url in browser

--- Exercise1/app/src/main/java/com/example/exercise1/SecondActivity.kt
+++ Exercise1/app/src/main/java/com/example/exercise1/SecondActivity.kt
@@ -1,6 +1,7 @@
 package com.example.exercise1
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_second.*
@@ -9,7 +10,8 @@ class SecondActivity : AppCompatActivity(R.layout.activity_second) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         txtvLink.setOnClickListener {
-            TODO("implicit intent")
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org"))
+            startActivity(intent)
         }
     }
 }
