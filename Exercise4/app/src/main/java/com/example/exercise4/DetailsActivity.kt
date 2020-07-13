a3a68a852baa04b45fec96fde53950da2200cd8d
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-13 14:33:57 +0300

    created details activity and layout

---
+++ Exercise4/app/src/main/java/com/example/exercise4/DetailsActivity.kt
@@ -0,0 +1,10 @@
+package com.example.exercise4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsActivity : AppCompatActivity(R.layout.activity_details) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
