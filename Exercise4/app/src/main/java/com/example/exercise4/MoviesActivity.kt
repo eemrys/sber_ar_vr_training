ff5101283d42787eb2fa53d4b851b59dde467447
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-13 13:53:30 +0300

    added data for recycler view

--- Exercise4/app/src/main/java/com/example/exercise4/MoviesActivity.kt
+++ Exercise4/app/src/main/java/com/example/exercise4/MoviesActivity.kt
@@ -2,10 +2,18 @@ package com.example.exercise4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_movies.*
 
-class MoviesActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity(R.layout.activity_movies) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movies)
+        val moviesAdapter = MoviesAdapter()
+        val manager = LinearLayoutManager(this)
+        moviesAdapter.data = createMovieList()
+        recyclervMovies.apply {
+            adapter = moviesAdapter
+            layoutManager = manager
+        }
     }
 }
