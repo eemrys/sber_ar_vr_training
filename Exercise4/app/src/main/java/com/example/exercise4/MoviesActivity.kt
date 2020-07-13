3fc2d9fbd24655e25bef4ff9f901ac32dba91f68
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-13 12:43:09 +0300

    added xml layouts for recycler view

--- Exercise4/app/src/main/java/com/example/exercise4/MoviesActivity.kt
+++ Exercise4/app/src/main/java/com/example/exercise4/MoviesActivity.kt
@@ -3,9 +3,9 @@ package com.example.exercise4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
     }
 }
