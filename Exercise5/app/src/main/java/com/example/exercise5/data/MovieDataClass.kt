06b05068f2998bd71336d4f8412f3a7e131606b3
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-14 19:51:09 +0300

    added layouts and other res files

---
+++ Exercise5/app/src/main/java/com/example/exercise5/data/MovieDataClass.kt
@@ -0,0 +1,11 @@
+package com.example.exercise5.data
+
+import android.os.Parcelable
+import androidx.annotation.DrawableRes
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Movie(val title: String,
+                 val summary: String,
+                 @DrawableRes val poster: Int,
+                 val trailerUrl: String) : Parcelable
