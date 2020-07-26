779eac01c08db0b4355c10cde85877cea43c58e1
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-26 21:25:24 +0300

    created background work fragment

---
+++ Exercise10/app/src/main/java/com/example/exercise10/work/BackgroundServiceFragment.kt
@@ -0,0 +1,7 @@
+package com.example.exercise10.work
+
+import androidx.fragment.app.Fragment
+import com.example.exercise10.R
+
+class BackgroundServiceFragment : Fragment(R.layout.fragment_background_service) {
+}
