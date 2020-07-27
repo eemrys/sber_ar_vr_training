0d944644bf5892ee27dd42dd90a1c0c7aa7de3d3
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-27 21:54:58 +0300

    added handler and runnable to services

--- Exercise10/app/src/main/java/com/example/exercise10/work/services/BackgroundJobIntentService.kt
+++ Exercise10/app/src/main/java/com/example/exercise10/work/services/BackgroundJobIntentService.kt
@@ -1,14 +1,43 @@
 package com.example.exercise10.work.services
 
+import android.app.Service
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import androidx.core.app.JobIntentService
 
 class BackgroundJobIntentService : JobIntentService() {
+
+    private var counter = 0
+    private val maxProgress: Int = 100
+    private val handler: Handler = Handler(Looper.getMainLooper())
+    private val runnable = Runnable {
+        showProgressNumber(
+            if (counter < maxProgress) {
+                counter++
+            } else {
+                counter
+            }
+        )
+    }
+
     override fun onHandleWork(intent: Intent) {
         TODO("Not yet implemented")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+        counter = 0
+        handler.postDelayed(runnable, 1000)
+        return Service.START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(runnable)
+    }
+
+    private fun showProgressNumber(progress: Int) {
+        //AndroidServiceDelegate.setProgressToService(progress)//to do inject
+        handler.postDelayed(runnable, 1000)
     }
 }
--- Exercise10/app/src/main/java/com/example/exercise10/work/services/BackgroundService.kt
+++ Exercise10/app/src/main/java/com/example/exercise10/work/services/BackgroundService.kt
@@ -3,11 +3,25 @@ package com.example.exercise10.work.services
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 
 class BackgroundService : Service() {
 
     private val binder = ServiceBinder()
+    private var counter = 0
+    private val maxProgress: Int = 100
+    private val handler: Handler = Handler(Looper.getMainLooper())
+    private val runnable = Runnable {
+        showProgressNumber(
+            if (counter < maxProgress) {
+                counter++
+            } else {
+                counter
+            }
+        )
+    }
 
     inner class ServiceBinder : Binder() {
         val service: BackgroundService
@@ -19,6 +33,18 @@ class BackgroundService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        counter = 0
+        handler.postDelayed(runnable, 1000)
         return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(runnable)
+    }
+
+    private fun showProgressNumber(progress: Int) {
+        //AndroidServiceDelegate.setProgressToService(progress)//to do inject
+        handler.postDelayed(runnable, 1000)
     }
 }
