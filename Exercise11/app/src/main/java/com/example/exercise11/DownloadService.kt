9d307cd543e233c9b9e627fb4326f7a0ec217e53
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-28 17:53:09 +0300

    created notification and channel

--- Exercise11/app/src/main/java/com/example/exercise11/DownloadService.kt
+++ Exercise11/app/src/main/java/com/example/exercise11/DownloadService.kt
@@ -1,17 +1,62 @@
 package com.example.exercise11
 
-import android.app.Service
+import android.app.*
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+private const val ONGOING_NOTIFICATION_ID = 987
+private const val CHANNEL_DEFAULT_IMPORTANCE = "01_Channel"
 
 class DownloadService : Service() {
+
+    private val notificationManager by lazy {
+        //NotificationManagerCompat
+        //createNotificationChannel
+    }
+
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val url = intent?.getStringExtra(URL)
+        //startForeground(ONGOING_NOTIFICATION_ID, createNotification())
         stopSelf()
         return START_STICKY
+    }
+
+    private fun createNotification(progress: Int): Notification {
+        val notificationIntent = Intent(this, ImageFragment::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        return NotificationCompat.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
+            .setContentTitle(getString(R.string.notification_title, progress))
+            .setContentText(getText(R.string.notification_message))
+            .setSmallIcon(R.drawable.ic_stat_download)
+            .setContentIntent(pendingIntent)
+            .build()
+    }
+
+    private fun createNotificationChannel() : NotificationChannel? {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = getString(R.string.channel_name)
+            val description = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            return NotificationChannel(
+                CHANNEL_DEFAULT_IMPORTANCE,
+                name,
+                importance
+            ).apply {
+                this.description = description
+                this.lightColor = Color.RED
+                this.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+                enableLights(true)
+                enableVibration(true)
+            }
+        }
+        return null
     }
 }
