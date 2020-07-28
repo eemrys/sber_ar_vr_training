1984475ee9ec28d0c1432a579f7038949473eb98
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-28 16:36:05 +0300

    added main fragment and layout

---
+++ Exercise11/app/src/main/java/com/example/exercise11/ImageFragment.kt
@@ -0,0 +1,17 @@
+package com.example.exercise11
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class ImageFragment : Fragment(R.layout.fragment_image) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListener()
+    }
+
+    private fun setListener() {
+        TODO("Not yet implemented")
+    }
+}
