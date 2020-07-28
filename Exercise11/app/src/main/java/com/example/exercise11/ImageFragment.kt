709c8d4bdb607a568a8c3bf2cb54ea131b8abf46
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-28 17:25:40 +0300

    handled permissions

--- Exercise11/app/src/main/java/com/example/exercise11/ImageFragment.kt
+++ Exercise11/app/src/main/java/com/example/exercise11/ImageFragment.kt
@@ -1,9 +1,17 @@
 package com.example.exercise11
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_image.*
+
+private const val PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE
+private const val PERMISSIONS_REQUEST_CODE = 1
 
 class ImageFragment : Fragment(R.layout.fragment_image) {
 
@@ -12,9 +20,48 @@ class ImageFragment : Fragment(R.layout.fragment_image) {
         setListener()
     }
 
+
     private fun setListener() {
         btnFab.setOnClickListener {
-            // start work
+            onFabClick()
         }
+    }
+
+    private fun onFabClick() {
+        if (isPermissionGranted()) {
+            startDownloadService()
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean =
+        ContextCompat.checkSelfPermission(requireContext(), PERMISSION) ==
+                PackageManager.PERMISSION_GRANTED
+
+    private fun startDownloadService() {
+        TODO("Not yet implemented")
+    }
+
+    private fun requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), PERMISSION)) {
+            showExplainingRationaleDialog()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(PERMISSION), PERMISSIONS_REQUEST_CODE)
+        }
+
+    }
+
+    private fun showExplainingRationaleDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle(R.string.dialog_title)
+            .setMessage(R.string.dialog_message)
+            .setPositiveButton(R.string.dialog_ok) {_, _ ->
+                ActivityCompat.requestPermissions(
+                    requireActivity(), arrayOf(PERMISSION), PERMISSIONS_REQUEST_CODE)
+            }
+            .create()
+            .show()
     }
 }
